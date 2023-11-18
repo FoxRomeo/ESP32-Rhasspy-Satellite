704d6dcc7b8852c7a1cf5f633b90342440d2ef34
Bernd Waldmann <requireiot@mail.de>
2023-11-18 20:09:36 +0100

    read mic as 32bit, discard 16 LSB

--- PlatformIO/src/devices/Inmp441Max98357a.hpp
+++ PlatformIO/src/devices/Inmp441Max98357a.hpp
@@ -5,24 +5,26 @@
 #include <driver/i2s.h>
 #include "IndicatorLight.h"
 
-// I2S pins for MIC
+// I2S pins & parameters for MIC
 #define MIC_I2S_BCLK    17
 #define MIC_I2S_FS      16
 #define MIC_I2S_DIN     18
 #define MIC_I2S_PORT    I2S_NUM_0
 #define MIC_SAMPLE_RATE   (16000)
+#define MIC_I2S_SAMPLE_BITS   32
+#define MIC_I2S_SAMPLE_BYTES  (MIC_I2S_SAMPLE_BITS/8)
+#define I2S_READ_LEN     (256*MIC_I2S_SAMPLE_BYTES)
 
-// I2S pins for speaker
+// I2S pins & parameters for speaker
 #define SPK_I2S_FS      27
 #define SPK_I2S_BCLK    14
 #define SPK_I2S_DOUT    12
 #define SPK_I2S_PORT    I2S_NUM_1
 #define SPK_SAMPLE_RATE (22050)
+#define SPK_I2S_SAMPLE_BITS   16
+#define SPK_I2S_SAMPLE_BYTES  (SPK_I2S_SAMPLE_BITS/8)
 
-#define I2S_SAMPLE_BITS   (16)
-#define I2S_READ_LEN     512
-
-// LEDs
+// GPIO pin used to drive LEDs
 #define LED_FLASH 4
 
 class Inmp441Max98357a : public Device
@@ -61,9 +63,9 @@ void Inmp441Max98357a::init() {
   i2s_config_t i2s_config_spk = {
       .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_TX),
       .sample_rate = SPK_SAMPLE_RATE,
-      .bits_per_sample = I2S_BITS_PER_SAMPLE_16BIT,  
+      .bits_per_sample = static_cast<i2s_bits_per_sample_t>(SPK_I2S_SAMPLE_BITS),  
       .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
-      .communication_format = (i2s_comm_format_t)(I2S_COMM_FORMAT_STAND_I2S ), 
+      .communication_format = I2S_COMM_FORMAT_STAND_I2S, 
       .intr_alloc_flags = ESP_INTR_FLAG_LEVEL1,
       .dma_buf_count = 2,
       .dma_buf_len = 512,
@@ -98,20 +100,22 @@ void Inmp441Max98357a::init() {
     - Even a loud speech signal won't exceed 80 dB, so we "waste" the top 40 dB 
       of dynamic range. 
     - The microphone sends 24 bit samples (which is not to say it has a true 
-       24 bit dynamic range). 
+      24 bit dynamic range). 
     - If we read 16 bit samples (nominal 96 dB dynamic range), and waste the top 
       40 dB, then we get a ~56 dB dynamic range speech signal.
-    - Might bet better to read 24 bit samples, and amplify by 40 dB
+    - Might bet better to read 24 bit samples, and amplify by 40 dB ?
+    - in my experiments, a x20 gain (26 dB) will result in a 50% FS signal, i.e.
+      not clipping, with a loud voice at 30cm distance
   */
   i2s_config_t i2s_config_mic = {
     .mode = (i2s_mode_t)(I2S_MODE_MASTER | I2S_MODE_RX),
     .sample_rate = MIC_SAMPLE_RATE,
-    .bits_per_sample = i2s_bits_per_sample_t(I2S_SAMPLE_BITS),
+    .bits_per_sample = static_cast<i2s_bits_per_sample_t>(MIC_I2S_SAMPLE_BITS),
     .channel_format = I2S_CHANNEL_FMT_ONLY_LEFT,
     .communication_format = I2S_COMM_FORMAT_STAND_I2S, 
     .intr_alloc_flags = 0,
     .dma_buf_count = 2,
-    .dma_buf_len = 512,
+    .dma_buf_len = I2S_READ_LEN,
     .use_apll = 1
   };
 
@@ -199,26 +203,30 @@ void Inmp441Max98357a::writeAudio(uint8_t *data, size_t size, size_t *bytes_writ
 
 bool Inmp441Max98357a::readAudio(uint8_t *data, size_t size) 
 {
+    size_t samples_requested = size / sizeof(int16_t);
+    size_t bytes_requested = MIC_I2S_SAMPLE_BYTES * samples_requested;
     size_t bytes_read;
-    i2s_read(MIC_I2S_PORT, (void*) i2s_read_buff, size, &bytes_read, portMAX_DELAY);
+    // we skip every other sample, so read 2x desired # of samples
+    i2s_read(MIC_I2S_PORT, (void*) i2s_read_buff, bytes_requested, &bytes_read, portMAX_DELAY);
     
     int16_t* from = (int16_t*) i2s_read_buff;
     int16_t* to   = (int16_t*) data;
-    size_t nsamples = bytes_read / sizeof(int16_t);
+    size_t nsamples = bytes_read / MIC_I2S_SAMPLE_BYTES;
     size_t i;
     int32_t y;
     /*
-      according to https://www.esp32.com/viewtopic.php?t=11023 
-      incoming samples are in flipped order, i.e. 1,0,3,2,5,4,...
+      according to https://www.esp32.com/viewtopic.php?t=11023 , incoming samples 
+      are in flipped order, i.e. 1,0,3,2,5,4,... instead of 0,1,2,3,4,5,...
+
+      As of ESP-IF 4, this is not true anymore. However, reading from the 
+      microphone as int16_t appears to repeat every sample twice, so we read
+      as int32_t instead, and ignore the 16 LSBs
     */
-    for (i=0; i<nsamples; i+=2) {
-        y = from[1];
+    for (i=0; i<nsamples; i++) {
+        y = from[1];    // read 16 MSB of the 32 bit value
         y *= m_gain;
         *to++ = y;
-        y = from[0];
-        y *= m_gain;
-        *to++ = y;
-        from += 2;        
+        from += 2;
     }
     return true;
 }
